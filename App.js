10891986de505927f0aac902d8a6a61d868236c2
DorianeFeya <49432825+DorianeFeya@users.noreply.github.com>
2020-04-06 12:44:37 +0100

    second commit

--- App.js
+++ App.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {Component} from 'react';
 
-export default function App() {
-  return (
+import { 
+    StyleSheet,
+  StatusBar,
+  View } from 'react-native';
+
+import Login from './src/pages/Login';
+import Signup from './src/pages/Signup';
+import Navigator from './src/Routes/LoginStack';
+
+export default class App extends Component<{}>{
+
+  render (){
+    return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar
+      backgroundColor="#263238"
+      barStyle="light-content"/>
+      <Login/>
     </View>
   );
+}
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+     backgroundColor: "#455A64",
+     alignItems: "center",
+     justifyContent: "center",
   },
 });
